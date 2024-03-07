9706ec2dcf2d39f7cfb927c3506a3d89c345ad2e
Danilo Vaccalluzzo <68116543+danilotrix86@users.noreply.github.com>
2024-03-07 10:05:50 +0100

    adding a few more options and improving the extraction

--- test.py
+++ test.py
@@ -1,61 +1,58 @@
 from nlp.openai_client import OpenAIClient
 import json
 
-keywords = "Cosa vedere a Marina di Ragusa, Marina di Ragusa, Ragusa mare"
+keywords = "how to clean your face, daily skincare routine, face cleaning, how?"
 topics_n = 5
-language = "italiano"
-prompt = f'''
-Given the keywords {keywords}, generate a comprehensive list of potential topics and subtopics for a detailed long-form article. 
-The article aims to cover various aspects of {keywords}, including practical advice, technological innovations, societal impacts, and personal stories. 
-Please include:
-- An overarching theme or angle for the article that ties all topics together.
-- {topics_n} Major topics that are essential for understanding {keywords}.
-- Subtopics under each major topic that delve into specific details, methods, challenges, and solutions.
-- Questions that the article should aim to answer for readers interested in {keywords}.
-- Any relevant trends, statistics, or case studies that could be included to support the content.
-- The goal is to provide a broad yet detailed framework that covers the multifaceted nature of {keywords}, appealing to readers at different stages of their interest or involvement.
-Return a json structure like this:
-
-{{
-    title,
-    topic,
-        subtopic,
-        subtopic,
-        subtopic,
-    topic,
-        subtopic,
-        subtopic,
-        subtopic,
-    topic,
-        subtopic,
-        subtopic,
-        subtopic,
-    questions_to_answer,
-    relevant_trends
-}}
+language = "english"
 
 
 
+###########################################################################################################################
+# EXTRACT THE TOPICS AND SUBTOPICS FOR A LONG-FORM ARTICLE
+###########################################################################################################################
+
+print ("Extracting topics and subtopics for a long-form article...")
+extract_topics_system = "You are a powerfull and experienced SEO and Content expert. You are specialized in extract topics and subtopics starting from keywords"
+extract_topics_user = f'''
+    Given a set of keywords: {keywords}, this function generates a comprehensive list of potential topics and subtopics
+    for a detailed long-form article. The article aims to cover various aspects related to the keywords,
+    including practical advice, technological innovations, societal impacts, and personal stories.
+
+    The function returns a JSON structure that includes:
+    - {topics_n} major topics that are essential for a thorough understanding of the given keywords.
+    - A series of subtopics under each major topic, detailing specific aspects, methods, challenges, and solutions.
+
+    The goal is to provide a broad yet detailed framework that captures the multifaceted nature of the topics,
+    appealing to readers at various stages of their interest or involvement.
+
+    Returns:
+    - A JSON structure with the specified number of topics and various subtopics for each, structured as follows:
+
+    {{
+        "topic": "the topic",
+            "subtopic": "the subtopic",
+            "subtopic": "the subtopic",
+            "subtopic": "the subtopic",
+        "topic": "the topic",
+            "subtopic": "the subtopic",
+            "subtopic": "the subtopic",
+            "subtopic": "the subtopic",
+        ...
+    }}
 '''
 
-json_structure = OpenAIClient("gpt-3.5-turbo", 0, True, 100)
+json_structure = OpenAIClient("gpt-3.5-turbo", 0, True)
+json_structure.add_to_message(extract_topics_system, "system")
+json_structure.add_to_message(extract_topics_user, "user")
 
-json_structure.add_to_message(prompt, "user")
+answer, usage = (json_structure.completions())
 
+topics_json = json.loads(answer)
 
-minchia, cazzi = (json_structure.completions())
-
-minchia_json = json.loads(minchia)
-
-minchia_json.pop("title", None)
-
-print (minchia_json.pop("topic", None))
+print (topics_json)
 
 
-json_structure.messages  = []
-json_structure.add_to_message(f"write a short article using this json structure {minchia_json} ", "user")
-answer, usage = json_structure.completions()
-
-print (answer)
-
+###########################################################################################################################
+# EXTRACT THE SEARCH INTENT FROM THE KEYWORDS
+###########################################################################################################################
 
